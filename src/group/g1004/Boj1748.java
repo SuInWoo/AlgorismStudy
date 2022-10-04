1246e3956d3ad045be275c28a23ea710707920fd
SuInWoo <wbo1026@naver.com>
2022-10-05 02:28:01 +0900

    백준 1748번: 수 이어 쓰기1 - 10,100,1000...으로 나누었을때 자리수를 더해줌

---
+++ src/group/g1004/Boj1748.java
@@ -0,0 +1,24 @@
+package group.g1004;
+
+import java.util.Scanner;
+
+public class Boj1748 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int digits = 1;
+        int div = 10;
+        int rnt = 0;
+        
+        for (int i = 1; i <= N; i++){
+            if(i % div == 0){
+                digits++;
+                div *= 10;
+            }
+            rnt += digits;
+        }
+
+        System.out.println(rnt);
+    }
+}
