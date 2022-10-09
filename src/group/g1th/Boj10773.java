58daecad78aa605328557bc8ca3e4b04be98ed93
SuInWoo <wbo1026@naver.com>
2022-10-09 22:50:39 +0900

    백준 10773번: 제로 - 값을 넣다가 0을 만나면 들어간 값 빼기 - stack사용

---
+++ src/group/g1th/Boj10773.java
@@ -0,0 +1,32 @@
+package group.g1th;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Boj10773 {
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> stack = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int K = Integer.parseInt(br.readLine());
+        int rnt = 0;
+
+        for (int i = 0; i < K; i++){
+            int in = Integer.parseInt(br.readLine());
+            if (in == 0){
+                stack.pop();
+            }else {
+                stack.push(in);
+            }
+        }
+
+        for (Integer integer : stack) {
+            rnt += integer;
+        }
+
+        System.out.printf("%d", rnt);
+
+    }
+}
