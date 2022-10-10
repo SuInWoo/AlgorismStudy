5780dc220fcc9789206540e1ee96b52475f65f8f
SuInWoo <wbo1026@naver.com>
2022-10-10 16:05:02 +0900

    백준 1057번: 토너먼트

---
+++ src/group/g1th/Boj1057.java
@@ -0,0 +1,21 @@
+package group.g1th;
+
+import java.util.Scanner;
+
+public class Boj1057 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int kim = sc.nextInt();
+        int im = sc.nextInt();
+        int count=0;
+
+        while(kim != im) {
+            kim = kim/2 + kim%2;
+            im = im/2 + im%2;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
