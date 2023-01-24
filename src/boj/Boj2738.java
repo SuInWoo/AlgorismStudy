66d5a8408f82b744f985350b63582a14d90e6eec
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 <wbo1026@naver.com>
2023-01-24 18:37:45 +0900

    백준 2738: 행렬 덧셈

---
+++ src/boj/Boj2738.java
@@ -0,0 +1,46 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj2738 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arrA = new int[n][m];
+        int[][] arrB = new int[n][m];
+        int[][] ans = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arrA[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arrB[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans[i][j] = arrA[i][j] + arrB[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
