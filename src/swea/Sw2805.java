73564f245e83580afc9d5032f75d8d60cccf17ec
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 <wbo1026@naver.com>
2023-01-30 22:46:45 +0900

    SWEA 2805: 농작물 수확하기

--- src/swea/Sw2805.java
+++ src/swea/Sw2805.java
@@ -2,7 +2,6 @@ package swea;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Sw2805 {
     public static void main(String[] args) throws Exception {
