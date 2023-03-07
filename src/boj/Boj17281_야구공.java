3b80532a7ab200ef7bc1b89ad9a6568f72934c99
SuIn <wbo1026@naver.com>
2023-03-07 17:52:04 +0900

    백준 17281: 야구공 - 수정 필요

---
+++ src/boj/Boj17281_야구공.java
@@ -0,0 +1,106 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj17281_야구공 {
+
+    static int N, ans, innings;
+    static int[][] score;
+    static boolean[] visited;
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+
+        score = new int[N][9];
+        visited = new boolean[9];
+        arr = new int[9];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                score[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // end input
+
+        perm(0);
+        System.out.println(ans);
+    }
+
+    static int gameStart() {
+        int atBat = 0; // 타석
+        int sum = 0;
+
+        // 총 이닝 수만큼 반복
+        while (innings != N) {
+            // 총 deathCnt 만큼 반복
+            int deathCnt = 3;
+            int[] base = new int[3]; // 1루, 2루, 3루
+
+            while (deathCnt != 0) {
+                int batter = arr[atBat % 9]; // 타자 꺼내고 타석 증가
+                atBat++;
+
+                int type = score[innings][batter]; // 현재 타자 상태
+
+                if (type == 0) { // 꺼낸 타자가 아웃이면
+                    deathCnt--;
+                } else if (type == 1) { // 안타이면
+                    sum += base[2];
+                    base[2] = base[1];
+                    base[1] = base[0];
+                    base[0] = 1;
+                } else if (type == 2) { // 2루타
+                    sum += base[2] + base[1]; // 3루, 2루 수는 홈으로
+                    base[2] = base[0]; // 1루는 3루로
+                    base[1] = 1; // 2루에 한명 넣기
+                    base[0] = 0; // 1루는 비워야함
+                } else if (type == 3) { // 3루타
+                    sum += base[2] + base[1] + base[0]; // 1루, 2루, 3루는 홈으로
+                    base[2] = 1; // 3루에 한명 나감
+                    base[1] = 0;
+                    base[0] = 0;
+                } else { // 홈런
+                    sum += base[2] + base[1] + base[0] + 1;
+                    base = new int[3];
+                }
+            }
+            innings++;
+        }
+
+        return sum;
+    }
+
+    static void perm(int depth) {
+        if (depth == 9) {
+            System.out.println(Arrays.toString(arr));
+            ans = Math.max(ans, gameStart());
+            return;
+        }
+
+        for (int i = 1; i < 9; i++) {
+            if (visited[i]) // 이미 타석에 들어섰으면
+                continue; // 넘어감
+
+            visited[i] = true;
+            arr[depth] = i; // 그 타석에 선수 넣음
+
+            if (depth + 1 == 3)
+                perm(depth + 2);
+            else
+                perm(depth + 1);
+
+            visited[i] = false;
+        }
+    }
+
+
+}
