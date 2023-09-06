5b0acba736e9010cf3359825c1579b26a9a7618d
SuIn Woo <wbo1026@naver.com>
2023-09-06 20:11:11 +0900

    Programmers: 멀리 뛰기

---
+++ src/programmers/P_멀리뛰기.java
@@ -0,0 +1,14 @@
+package programmers;
+
+class P_멀리뛰기 {
+    public long solution(int n) {
+        int[] dp = new int[2001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<2001; i++){
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}
