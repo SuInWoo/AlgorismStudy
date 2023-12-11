9d02cfe8f9eddb88e19e74999a1414a280447f60
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 <wbo1026@naver.com>
2023-12-11 20:17:02 +0900

    Programmers: 이중우선순위큐

--- src/programmers/P_이중우선순위큐.java
+++ src/programmers/P_이중우선순위큐.java
@@ -7,33 +7,33 @@ class P_이중우선순위큐 {
         PriorityQueue<Integer> min = new PriorityQueue<>();
         PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
         StringTokenizer st;
-        
-        for(int i=0;i<operations.length;i++) {
+
+        for (int i = 0; i < operations.length; i++) {
             st = new StringTokenizer(operations[i]);
             char op = st.nextToken().charAt(0);
             int num = Integer.parseInt(st.nextToken());
-            
-            switch(op) {
+
+            switch (op) {
                 case 'I':
                     min.add(num);
                     max.add(num);
                     break;
                 case 'D':
-                    if(max.isEmpty()) break;
-                    if(num == 1) {
+                    if (max.isEmpty()) break;
+                    if (num == 1) {
                         int del = max.poll();
                         min.remove(del);
                     }
-                    if(num == -1) {
+                    if (num == -1) {
                         int del = min.poll();
                         max.remove(del);
                     }
             }
         }
-        
-        if(max.isEmpty())
-            return new int[] {0, 0};
-        
-        return new int[] {max.peek(), min.peek()};
+
+        if (max.isEmpty())
+            return new int[]{0, 0};
+
+        return new int[]{max.peek(), min.peek()};
     }
 }
