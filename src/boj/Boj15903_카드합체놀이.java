108f54c692470771578c5aee4cb483e8b5113a73
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 <wbo1026@naver.com>
2023-12-27 18:37:35 +0900

    백준 15903: 카드 합체 놀이

---
+++ src/boj/Boj15903_카드합체놀이.java
@@ -0,0 +1,41 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Boj15903_카드합체놀이 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // init
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        long sum = 0;
+        PriorityQueue<Long> pq = new PriorityQueue<>(); // 작은 것부터 출력
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            pq.offer((long) Integer.parseInt(st.nextToken())); // pq에 집어넣기
+        }
+
+        // logic
+        for (int i = 0; i < m; i++) {
+            long min1 = pq.poll();
+            long min2 = pq.poll();
+
+            pq.offer(min1 + min2);
+            pq.offer(min1 + min2);
+        }
+
+        while(!pq.isEmpty()) {
+            sum += pq.poll();
+        }
+
+        System.out.println(sum);
+
+    }
+}
