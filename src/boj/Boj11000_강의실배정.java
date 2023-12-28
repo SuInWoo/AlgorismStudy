215490e35de1304658b3533338223dcb275f6883
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 <wbo1026@naver.com>
2023-12-28 16:06:17 +0900

    백준 11000: 강의실 배정

--- src/boj/Boj11000_강의실배정.java
+++ src/boj/Boj11000_강의실배정.java
@@ -9,31 +9,28 @@ public class Boj11000_강의실배정 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // init
+        // 초기화
         int N = Integer.parseInt(br.readLine());
-        ArrayList<Subject> list = new ArrayList<>(); // 시작 시간으로 강의 정렬
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 제일 빨리 끝나는 시간이 언제인지 알려주는 pq
+        PriorityQueue<Subject> pqSubjects = new PriorityQueue<>(); // 강의들을 관리할 우선순위 큐
+        PriorityQueue<Integer> pqEndTimes = new PriorityQueue<>(); // 강의 종료 시간을 관리할 우선순위 큐
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            list.add(new Subject(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            pqSubjects.offer(new Subject(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
 
-        // 정렬
-        Collections.sort(list);
+        // 우선순위 큐에 강의가 있는 동안
+        while (!pqSubjects.isEmpty()) {
+            Subject sj = pqSubjects.poll(); // 가장 빨리 시작하는 강의를 가져옴
 
-        for (int i = 0; i < N; i++) {
-            Subject sj = list.get(i);
-
-            if (!pq.isEmpty() && pq.peek() <= sj.start) { // 아무 강의도 하지않거나, 강의 시간이 종료되었을때
-                pq.poll();
+            if (!pqEndTimes.isEmpty() && pqEndTimes.peek() <= sj.start) { // 강의실이 비어 있거나 강의가 종료되었을 때
+                pqEndTimes.poll(); // 강의실을 비워줌
             }
 
-            pq.offer(sj.end); // 강의 종료시간 넣어주기
-
+            pqEndTimes.offer(sj.end); // 현재 강의의 종료 시간을 추가
         }
 
-        System.out.println(pq.size());
-
+        System.out.println(pqEndTimes.size()); // 필요한 강의실의 수는 pqEndTimes의 크기
     }
 
     static class Subject implements Comparable<Subject> {
@@ -54,3 +51,4 @@ public class Boj11000_강의실배정 {
         }
     }
 }
+
